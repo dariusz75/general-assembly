d0f0fe9cab16869b201aca4dce6b7e4efe97f236
Dariusz Franczak <darius32@op.pl>
2018-03-17 20:54:01 +0000

    wip

--- sites/firstApp/index.js
+++ sites/firstApp/index.js
@@ -1,5 +1,76 @@
+/*
 console.log('Node is working');
 var friends = [{name: 'Tom'}, {name: 'Fred'}, {name: 'Dave'}];
 friends.forEach(function(friend){
   console.log(friend.name);
+});
+*/
+
+var prompt = require('prompt');
+
+var users = [
+  {
+    cardNumber: 101010,
+    pinNumber: 1111,
+    balance: 1000
+  },
+  {
+    cardNumber: 202020,
+    pinNumber: 2222,
+    balance: 2000
+  },
+  {
+    cardNumber: 303030,
+    pinNumber: 3333,
+    balance: 3000
+  },
+];
+
+
+
+
+//
+// Start the prompt
+//
+prompt.start();
+
+//
+// Get two properties from the user: username and email
+//
+prompt.get(['pinNumber', 'cardNumber'], function (err, result) {
+  //
+  // Log the results.
+  //
+  //console.log('Command-line input received:');
+  //console.log('  pin number: ' + result.pinNumber);
+  //console.log('  email: ' + result.email);
+  var counter = 0;
+
+/*
+  users.forEach(function(user) {
+    if (result.cardNumber == user.cardNumber && result.pinNumber == user.pinNumber) {
+      console.log(user.balance);
+    } else if (result.cardNumber != user.cardNumber || result.pinNumber != user.pinNumber){
+      console.log('PIN number doesn\'t match the card number. Please provide the correct PIN number.');
+      counter++;
+      console.log(counter);
+    }
+  });
+*/
+
+
+
+switch(result.pinNumber) {
+    case '1111':
+        console.log('chuj 1');
+        break;
+    case '2222':
+        console.log('chuj 2');
+        break;
+    case '3333':
+        console.log('chuj 3');
+        break;
+
+}
+
 });
