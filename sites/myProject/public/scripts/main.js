db9d8e241e5a4b1eab0079216eeda385b93417db
Dariusz Franczak <darius32@op.pl>
2018-04-07 11:48:55 +0100

    data received

--- sites/myProject/public/scripts/main.js
+++ sites/myProject/public/scripts/main.js
@@ -1,16 +1,19 @@
-var source   = $("#profile-template").html();
+var toPlace = document.getElementById("ajax-content");
+var source = document.getElementById("profile-template").innerHTML;
 var template = Handlebars.compile(source);
 
 $.ajax({
   url: '//localhost:3000/cars'
 }).done(function(response){
-  //create HTML
-  var html = template(response);
-
-  //insert new HTML
-  $('#ajax-content').html(html);
+  response.forEach(function(respond){
+  var HTML = template(respond);
+  var listHTML = toPlace.innerHTML; //get current html
+  var newHTML = listHTML + HTML; //edit what ive copied
+  toPlace.innerHTML = newHTML; //write new stuff
+});
   console.log(response);
 });
+
 
 
 
