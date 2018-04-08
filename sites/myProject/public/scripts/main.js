a225d8f8411ad7878ce0744e0d899546b1d3a1c6
Dariusz Franczak <darius32@op.pl>
2018-04-08 22:46:47 +0100

    work in progress

--- sites/myProject/public/scripts/main.js
+++ sites/myProject/public/scripts/main.js
@@ -1,20 +1,75 @@
+
+var updating = false;
+var carToBeUpdeted = null;
+
+var $carsList = ("ajax-content");
+var $carCountIndicator = ("car-count");
+var $formTitle = $('.form-title');
+var $originalFormTitle = $('.form-title').text();
+var $submitButtotn = $('.submit-btn');
+var $submitButtotnText = $('.submit-btn').text();
+var $form = $('#car-form');
+var $makeInput = $form.find('#make');
+var $bhpInput = $form.find('#bhp');
+var $yearInput = $form.find('#year');
+
+var $activeButtonClass = 'active';
+
+//handlebars templating
 var toPlace = document.getElementById("ajax-content");
 var source = document.getElementById("profile-template").innerHTML;
-var template = Handlebars.compile(source);
+var templateFn = Handlebars.compile(source);
 
+var _cars = [];
+
+
+
+
+function resetiForm() {
+  $form.find('input[type="text"], input[type="number"]').val('');
+}
+
+function removeCar(id) {
+  var i = _cars.findIndex(function(car){
+    return id === car._id;
+  });
+  _car.splice(i, 1);
+}
+
+function updateCar(car, data) {
+  var i = _cars.findIndex(function(car){
+    return car._id === carToBeUpdeted._id;
+  });
+  _cars[i] = Object.assign({}, _cars[i], data);
+}
+
+function updateUI() {
+  $carsList.html='';
+  $carCountIndicator.text = _cars.length;
+  _cars.forEach(function(car){
+    $carsList.innerHTML += templateFn(car);
+  });
+}
+
+function handleAjaxFail (err) {
+  console.log(err.message);
+  alert('Ajax failed!');
+}
+
+//GET
 $.ajax({
   url: '//localhost:3000/cars/'
-}).done(function(response){
-  response.forEach(function(respond){
-  var HTML = template(respond);
-  var listHTML = toPlace.innerHTML; //get current html
-  var newHTML = listHTML + HTML; //edit what ive copied
-  toPlace.innerHTML = newHTML; //write new stuff
-});
-  console.log(response);
-});
+})
+.done(function(cars){
+  _cars = cars;
+  updateUI();
+})
+.fail(handleAjaxFail);
 
-
+$form.on('submit.addOrUpdate', function(e){
+  e.preventDefault();
+  console.log('bingo');
+})
 
 
 
